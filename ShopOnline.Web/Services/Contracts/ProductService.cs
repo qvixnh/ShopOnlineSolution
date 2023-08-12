6e970f0ceca08d0136385a7a367617d81e6e6ba7
qvixnh <nguyenvinh22012002@gmail.com>
2023-08-12 13:10:12 +0700

    Building Product Details api and blazor component DisplayError.Razor component to display error occuring when calling request

--- ShopOnline.Web/Services/Contracts/ProductService.cs
+++ ShopOnline.Web/Services/Contracts/ProductService.cs
@@ -11,13 +11,54 @@ namespace ShopOnline.Web.Services.Contracts
         {
             this.httpClient = httpClient;
         }
+
+        public async Task<ProductDto> GetItem(int id)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"api/Product/{id}");
+
+                if(response.IsSuccessStatusCode)
+                {
+                    if(response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(ProductDto); 
+                    }
+                    //translate json to object
+                    return await response.Content.ReadFromJsonAsync<ProductDto>();
+                }
+                else//handle exception
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception(message);
+                }
+            }
+            catch (Exception)
+            {
+                //Log exception
+                throw;
+            }
+        }
+
         public async Task<IEnumerable<ProductDto>> GetItems()
         {
             try
             {
                 //call api action method   
-                var products = await this.httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
-                return products;
+                var response= await httpClient.GetAsync("api/Product");
+                if (response.IsSuccessStatusCode)
+                {
+                    if(response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return Enumerable.Empty<ProductDto>();
+                    }
+                    return await response.Content.ReadFromJsonAsync<IEnumerable<ProductDto>>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception(message);
+                }
             }catch(Exception)
             {
                 throw;
