7731cef9f85b9d6dcdafc2e81248cc741c01ff39
qvixnh <nguyenvinh22012002@gmail.com>
2023-08-19 09:43:38 +0700

    Display and update "TotalPrice && Quantity"

--- ShopOnline.Web/Pages/ShoppingCartBase.cs
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -4,29 +4,53 @@ using ShopOnline.Web.Services.Contracts;
 
 namespace ShopOnline.Web.Pages
 {
-    public class ShoppingCartBase:ComponentBase
+    public class ShoppingCartBase : ComponentBase
     {
         [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
 
         public List<CartItemDto> ShoppingCartItems { get; set; } //IEnumerable can not remove item from client side collection -> list
-        public string ErrorMessage { get;set ; }
+        public string ErrorMessage { get; set; }
+
+        protected string TotalPrice { get; set; }
+        protected int TotalQuantity { get; set; }
         protected override async Task OnInitializedAsync()
         {
             try
             {
-                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+                    ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+                CalculateCartSummmaryTotals();
             }
             catch (Exception ex)
             {
                 ErrorMessage = ex.Message;
             }
         }
+        //set method
+        private void SetTotalPrice()
+        {
+            TotalPrice = this.ShoppingCartItems.Sum(p => p.TotalPrice).ToString();
+        }
+        private void SetTotalQuantity()
+        {
+            TotalQuantity = this.ShoppingCartItems.Sum(p => p.Qty);
+        }
+        private void UpdateItemTotalPrice(CartItemDto cartItemDto)
+        {
+            var item = GetCartItem(cartItemDto.Id);
+            if(item != null)
+            {
+                item.TotalPrice = cartItemDto.Price * cartItemDto.Qty; 
+            }
+        }
+
         //handle deleting cart item
         protected async Task DeleteCartItem_Click(int id)
         {
             var cartItemDto = await ShoppingCartService.DeleteItem(id);
             RemoveCartItem(id);
+            CalculateCartSummmaryTotals();
         }
         private CartItemDto GetCartItem(int id)
         {
@@ -36,6 +60,11 @@ namespace ShopOnline.Web.Pages
         {
             var cartItemDto = GetCartItem(id);
             ShoppingCartItems.Remove(cartItemDto);
+        }
+        private void CalculateCartSummmaryTotals()
+        {
+            SetTotalPrice();
+            SetTotalQuantity();
         }
         protected async Task UpdateQtyCartItem_Click(int id, int qty)
         {
@@ -49,6 +78,8 @@ namespace ShopOnline.Web.Pages
                         Qty = qty
                     };
                     var returnedUpateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
+                    UpdateItemTotalPrice(returnedUpateItemDto);
+                    CalculateCartSummmaryTotals();
                 }
                 else
                 {
